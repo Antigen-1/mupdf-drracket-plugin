9f45489d60f55cd4a5b8f330d2b133e27fd84f85
ZhangHao <zhanghao@antigen.top>
2024-09-20 01:17:34 +0800

    Fixes

--- scripts/display.rkt
+++ scripts/display.rkt
@@ -25,7 +25,7 @@
 
 ;; Code here
 
-(require quickscript racket/gui/base racket/class racket/vector racket/match racket-mupdf)
+(require quickscript racket/gui/base racket/class racket/vector racket/match racket/case racket-mupdf)
 
 (define-script pdf-display-script
   #:label "PDF displayer"
@@ -37,20 +37,19 @@
     (let/cc cc
       (define open-document/ctx (open-document (make-context)))
 
-      (define cont (box #f))
-
       (define mupdf-canvas%
         (class canvas%
           (super-new)
           (define/override (on-char evt)
             (collect-garbage 'incremental)
             (let/cc ex
-              ((unbox cont)
-               (case (send evt get-key-code)
+              ((unbox cont-box)
+               (case/eqv (send evt get-key-code)
                  ((left) 'left)
                  ((right) 'right)
                  ((up) 'up)
                  ((down) 'down)
+                 ((return) 'refresh)
                  ((#\a) 'last)
                  ((#\d) 'next)
                  ((#\w) 'zoom-in)
@@ -64,7 +63,8 @@
 
       (define frame (new frame% [label "mupdf"] [min-width 800] [min-height 600]))
       (define canvas (new mupdf-canvas% [parent frame] [style '(hscroll vscroll)]
-                          [min-width 800] [min-height 600]))
+                          [min-width 800] [min-height 600]
+                          [paint-callback (lambda (c d) ((unbox cont-box) 'refresh))]))
       (define dc (send canvas get-dc))
 
       (send dc set-smoothing 'aligned)
@@ -77,21 +77,20 @@
                          '(common) '(("PDF files" "*.pdf")))
                => open-document/ctx)
               (else (cc (void)))))
-      (define (init-renderer)
+      (define (new-renderer)
         (define nd (open-pdf-document))
         (define ncnt (document-count-pages nd))
-        (define ncont ((render nd ncnt)))
-        (set-box! cont ncont)
-        (ncont 'reset-settings))
+        ((render nd ncnt)))
+      (define (vector-update vec pos proc)
+        (define nv (vector-copy vec))
+        (vector-set! nv pos (proc (vector-ref vec pos)))
+        nv)
       (define (((render doc cnt) (cursor 0) (zoom1 1.0) (zoom2 1.0) (rotate 0.0) (x 0.0) (y 0.0)) instruction)
         (let/cc ex
           (define vec (vector cursor zoom1 zoom2 rotate x y))
-          (define (vector-update vec pos proc)
-            (define nv (vector-copy vec))
-            (vector-set! nv pos (proc (vector-ref vec pos)))
-            nv)
           (match-let (((vector nc nz1 nz2 nr nx ny)
-                       (case instruction
+                       (case/eq instruction
+                         ((refresh) vec)
                          ((last) (vector-update vec 0 (lambda (n) (if (zero? n) n (sub1 n)))))
                          ((next) (vector-update vec 0 (lambda (n) (if (>= n (sub1 cnt)) n (add1 n)))))
                          ((zoom-in) (vector-update (vector-update vec 1 (lambda (n) (+ n 0.01))) 2 (lambda (n) (+ n 0.01))))
@@ -103,17 +102,21 @@
                          ((up) (vector-update vec 5 (lambda (n) (- n 10.0))))
                          ((down) (vector-update vec 5 (lambda (n) (+ n 10.0))))
                          ((reset-settings) (vector cursor 1.0 1.0 0.0 0.0 0.0))
-                         ((next-session) (ex (init-renderer))))))
+                         ((next-session)
+                          (define ncont (new-renderer))
+                          (set-box! cont-box ncont)
+                          (ex (ncont 'refresh))))))
 
             ;; Rendering
             (send dc clear)
             ;; We use racket-side bitmap rotation
             (send dc set-rotation nr)
-            (send dc draw-bitmap (pixmap->bitmap ((extract-pixmap doc (make-matrix nz1 nz2 0.0)) cursor)) nx ny)
+            (send dc draw-bitmap (pixmap->bitmap ((extract-pixmap doc (make-matrix nz1 nz2 0.0)) nc)) nx ny)
             (send canvas flush)
 
-            (set-box! cont ((render doc cnt) nc nz1 nz2 nr nx ny)))))
+            (set-box! cont-box ((render doc cnt) nc nz1 nz2 nr nx ny)))))
 
-      (init-renderer)
+      ;; (box/c (-> symbol? any))
+      (define cont-box (box (new-renderer)))
 
       (send frame show #t))))
