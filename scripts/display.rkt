ab08fc2b0a14aa74b3fa96a392adc3aead5bdd1d
ZhangHao <zhanghao@antigen.top>
2024-09-21 01:03:04 +0800

    Use the CS architecture

--- scripts/display.rkt
+++ scripts/display.rkt
@@ -25,7 +25,7 @@
 
 ;; Code here
 
-(require quickscript racket/gui/base racket/class racket/vector racket/match racket/case racket-mupdf)
+(require quickscript racket/gui/base racket/class racket/match racket/case racket/async-channel racket-mupdf)
 
 (define-script pdf-display-script
   #:label "PDF displayer"
@@ -37,13 +37,16 @@
     (let/cc cc
       (define open-document/ctx (open-document (make-context)))
 
+      (define event-channel (make-async-channel))
+
       (define mupdf-canvas%
         (class canvas%
           (super-new)
           (define/override (on-char evt)
             (collect-garbage 'incremental)
             (let/cc ex
-              ((unbox cont-box)
+              (async-channel-put
+               event-channel
                (case/eqv (send evt get-key-code)
                  ((left) 'left)
                  ((right) 'right)
@@ -64,7 +67,7 @@
       (define frame (new frame% [label "mupdf"] [min-width 800] [min-height 600]))
       (define canvas (new mupdf-canvas% [parent frame] [style '(hscroll vscroll)]
                           [min-width 800] [min-height 600]
-                          [paint-callback (lambda (c d) ((unbox cont-box) 'refresh))]))
+                          [paint-callback (lambda (c d) (async-channel-put event-channel 'refresh))]))
       (define dc (send canvas get-dc))
 
       (send dc set-smoothing 'aligned)
@@ -72,51 +75,55 @@
       (send canvas enable #t)
       (send canvas focus)
 
+      (struct states (doc cnt cursor zoom1 zoom2 rotate x y) #:constructor-name make-states)
+
+      ;; Auxiliary functions
       (define (open-pdf-document)
         (cond ((get-file "Please choose a PDF file" frame #f #f ".pdf"
                          '(common) '(("PDF files" "*.pdf")))
                => open-document/ctx)
               (else (cc (void)))))
-      (define (new-renderer)
-        (define nd (open-pdf-document))
-        (define ncnt (document-count-pages nd))
-        ((render nd ncnt)))
-      (define (vector-update vec pos proc)
-        (define nv (vector-copy vec))
-        (vector-set! nv pos (proc (vector-ref vec pos)))
-        nv)
-      (define (((render doc cnt) (cursor 0) (zoom1 1.0) (zoom2 1.0) (rotate 0.0) (x 0.0) (y 0.0)) instruction)
-        (let/cc ex
-          (define vec (vector cursor zoom1 zoom2 rotate x y))
-          (match-let (((vector nc nz1 nz2 nr nx ny)
-                       (case/eq instruction
-                         ((refresh) vec)
-                         ((last) (vector-update vec 0 (lambda (n) (if (zero? n) n (sub1 n)))))
-                         ((next) (vector-update vec 0 (lambda (n) (if (>= n (sub1 cnt)) n (add1 n)))))
-                         ((zoom-in) (vector-update (vector-update vec 1 (lambda (n) (+ n 0.01))) 2 (lambda (n) (+ n 0.01))))
-                         ((zoom-out) (vector-update (vector-update vec 1 (lambda (n) (- n 0.01))) 2 (lambda (n) (- n 0.01))))
-                         ((rotate1) (vector-update vec 3 (lambda (n) (- n 0.15))))
-                         ((rotate2) (vector-update vec 3 (lambda (n) (+ n 0.15))))
-                         ((left) (vector-update vec 4 (lambda (n) (- n 10.0))))
-                         ((right) (vector-update vec 4 (lambda (n) (+ n 10.0))))
-                         ((up) (vector-update vec 5 (lambda (n) (- n 10.0))))
-                         ((down) (vector-update vec 5 (lambda (n) (+ n 10.0))))
-                         ((reset-settings) (vector cursor 1.0 1.0 0.0 0.0 0.0))
-                         ((next-session)
-                          (define ncont (new-renderer))
-                          (set-box! cont-box ncont)
-                          (ex (ncont 'refresh))))))
+      (define (new-session-states)
+        (define doc (open-pdf-document))
+        (define cnt (document-count-pages doc))
+        (make-states doc cnt 0 1.0 1.0 0.0 0.0 0.0))
+      (define (update-states sts instruction)
+        (case/eq instruction
+                 ((refresh) sts)
+                 ((last) (struct-copy states sts (cursor ((lambda (n) (if (zero? n) n (sub1 n))) (states-cursor sts)))))
+                 ((next) (struct-copy states sts (cursor ((lambda (n) (if (>= n (sub1 (states-cnt sts))) n (add1 n))) (states-cursor sts)))))
+                 ((zoom-in) (struct-copy states sts
+                                         (zoom1 ((lambda (n) (+ n 0.01)) (states-zoom1 sts)))
+                                         (zoom2 ((lambda (n) (+ n 0.01)) (states-zoom2 sts)))))
+                 ((zoom-out) (struct-copy states sts
+                                          (zoom1 ((lambda (n) (- n 0.01)) (states-zoom1 sts)))
+                                          (zoom2 ((lambda (n) (- n 0.01)) (states-zoom2 sts)))))
+                 ((rotate1) (struct-copy states sts (rotate ((lambda (n) (- n 0.15)) (states-rotate sts)))))
+                 ((rotate2) (struct-copy states sts (rotate ((lambda (n) (+ n 0.15)) (states-rotate sts)))))
+                 ((left) (struct-copy states sts (x ((lambda (n) (- n 10.0)) (states-x sts)))))
+                 ((right) (struct-copy states sts (x ((lambda (n) (+ n 10.0)) (states-x sts)))))
+                 ((up) (struct-copy states sts (y ((lambda (n) (- n 10.0)) (states-y sts)))))
+                 ((down) (struct-copy states sts (y ((lambda (n) (+ n 10.0)) (states-y sts)))))
+                 ((reset-settings) (make-states (states-doc sts) (states-cnt sts) (states-cursor sts) 1.0 1.0 0.0 0.0 0.0))
+                 ((next-session) (new-session-states))))
+      (define (render sts)
+        (match-let (((states doc cnt cursor zoom1 zoom2 rotate x y) sts))
+          ;; Rendering
+          (send dc clear)
+          ;; We use racket-side bitmap rotation
+          (send dc set-rotation rotate)
+          (send dc draw-bitmap (pixmap->bitmap ((extract-pixmap doc (make-matrix zoom1 zoom2 0.0)) cursor)) x y)
+          (send canvas flush)))
 
-            ;; Rendering
-            (send dc clear)
-            ;; We use racket-side bitmap rotation
-            (send dc set-rotation nr)
-            (send dc draw-bitmap (pixmap->bitmap ((extract-pixmap doc (make-matrix nz1 nz2 0.0)) nc)) nx ny)
-            (send canvas flush)
-
-            (set-box! cont-box ((render doc cnt) nc nz1 nz2 nr nx ny)))))
-
-      ;; (box/c (-> symbol? any))
-      (define cont-box (box (new-renderer)))
+      ;; The main thread is used as the server thread
+      (define client-thread
+        (thread (lambda ()
+                  (let loop ((sts (new-session-states)))
+                    (sync (handle-evt
+                           event-channel
+                           (lambda (ins)
+                             (define ns (update-states sts ins))
+                             (render ns)
+                             (loop ns))))))))
 
       (send frame show #t))))
